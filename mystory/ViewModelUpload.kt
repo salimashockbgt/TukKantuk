803ab45194bfe943cf1840e8a5e9c4aee9700263
salimashockbgt <76551955+salimashockbgt@users.noreply.github.com>
2023-12-08 16:45:30 +0700

    Delete mystory directory

--- mystory/ViewModelUpload.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mystory
-
-import androidx.lifecycle.ViewModel
-import java.io.File
-
-class ViewModelUpload(private val repository: Repository) : ViewModel() {
-
-    fun uploadImage(file: File, description: String) = repository.uploadImage(file, description)
-}
